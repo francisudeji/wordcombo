0f9726b399888840cff81a2d77620423d84bd118
Francis Udeji <francis.udejiofficial@gmail.com>
2024-12-05 16:44:15 +0100

    Remove build dir

--- build/client/assets/with-props-ByuiTVR5.js
+++
@@ -1,1 +0,0 @@
-import{t,v as e,w as o,x as p,r as s,y as u}from"./chunk-D52XG6IA-MGfthkNT.js";function i(a){return function(){const r={params:t(),loaderData:e(),actionData:o(),matches:p()};return s.createElement(a,r)}}function m(a){return function(){const r={params:t(),loaderData:e(),actionData:o(),error:u()};return s.createElement(a,r)}}export{m as a,i as w};
