a3f3d90b3d4c73bb338f1087a972eab15d05b880
Francis Udeji <francis.udejiofficial@gmail.com>
2024-12-17 13:05:29 +0100

    remove loader

--- app/routes/home/home.tsx
+++ app/routes/home/home.tsx
@@ -5,23 +5,18 @@ export function meta(): Route.MetaDescriptors {
   return [{ title: "WordCombo" }];
 }
 
-export function loader({ context }: Route.LoaderArgs) {
-  return {
-    message: context.VALUE_FROM_CLOUDFLARE,
-    title: "WordCombo",
-    description: "An online word game inspired by Wordle",
-  };
-}
-
-export default function Home({ loaderData }: Route.ComponentProps) {
-  const { title, description } = loaderData;
+export default function Home() {
   return (
     <div className="h-screen flex flex-col items-center justify-center text-center">
-      <h1 className="font-semibold text-lg text-neutral-950">{title}</h1>
-      <p className="text-neutral-600">{description}</p>
+      <h1 className="font-semibold text-lg text-neutral-950">WordCombo</h1>
+      <p className="text-neutral-600">An online word game inspired by Wordle</p>
 
       <div className="flex items-center space-x-2">
-        <NavLink to="/play" className="mt-4 hover:underline text-blue-500">
+        <NavLink
+          prefetch="intent"
+          to="/play"
+          className="mt-4 hover:underline text-blue-500"
+        >
           Play
         </NavLink>
         <span className="text-neutral-600 inline-flex self-end">|</span>
