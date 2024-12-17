3285e6b0386744d886d1a25c9105fcf7d9a15733
Francis Udeji <francis.udejiofficial@gmail.com>
2024-12-17 12:50:38 +0100

    Make IconButton component receive className and onClick props

--- app/routes/play/components/button/icon-button.tsx
+++ app/routes/play/components/button/icon-button.tsx
@@ -1,16 +1,26 @@
 import { type PropsWithChildren } from "react";
+import { cn } from "~/lib/utils";
 
 interface IconButtonProps {
   title: string;
+  className?: string;
+  onClick?: () => void;
 }
 export function IconButton({
   title,
+  className = "",
+  onClick,
   children,
 }: PropsWithChildren<IconButtonProps>) {
   return (
     <button
       title={title}
-      className={`font-normal rounded-md py-2 flex items-center justify-center text-center text-xl`}
+      type="button"
+      className={cn(
+        "font-normal rounded-md py-2 flex items-center justify-center text-center",
+        className
+      )}
+      onClick={onClick}
     >
       <span className="sr-only">{title}</span>
       {children}
