ec4c1d1fbf483118ebf0c7491c7000620d92633b
Francis Udeji <francis.udejiofficial@gmail.com>
2024-12-17 12:50:00 +0100

    Update UI for current word

---
+++ app/routes/play/components/current-word/current-word.tsx
@@ -0,0 +1,16 @@
+import { useGameState } from "../../hooks/use-game";
+
+export function CurrentWord() {
+  const currentWord = useGameState<"currentWord">((state) => state.currentWord);
+
+  return Array.from({ length: 5 }).map((_, index) => {
+    return (
+      <span
+        key={index}
+        className="text-xl py-2 px-4 rounded-lg border border-dashed font-medium h-[50px] w-[50px] flex items-center justify-center"
+      >
+        {currentWord[index] ?? " "}
+      </span>
+    );
+  });
+}
